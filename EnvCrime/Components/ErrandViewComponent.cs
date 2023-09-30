847cb986744d3487e1e903afed22e676d1fe2331
Rokas Paulauskas <rokas_nkt@yahoo.com>
2023-10-01 00:20:53 +0300

    Skapade ett ViewComponent och bytte ut hårdkodade delar av Crime vyer mot ett anrop på ViewComponent:et

---
+++ EnvCrime/Components/ErrandViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnvCrime.Components
+{
+	public class ErrandViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke(string roleName)
+		{ 
+			ViewBag.RoleName = roleName;
+			return View("SpecificErrand");
+		}
+	}
+}
